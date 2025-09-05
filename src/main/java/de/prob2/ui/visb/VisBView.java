80e540eb795bc56027ff90a4b29a0da772867912
DerHyper <maxrichter15@web.de>
2025-09-05 15:40:00 +0200

    Add VisBServer

--- src/main/java/de/prob2/ui/visb/VisBView.java
+++ src/main/java/de/prob2/ui/visb/VisBView.java
@@ -54,6 +54,7 @@ import de.prob2.ui.internal.executor.FxThreadExecutor;
 import de.prob2.ui.menu.ExternalEditor;
 import de.prob2.ui.prob2fx.CurrentProject;
 import de.prob2.ui.prob2fx.CurrentTrace;
+import static de.prob2.ui.visb.VisBServer.startServer;
 import de.prob2.ui.visb.help.UserManualStage;
 import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
@@ -754,7 +755,8 @@ public final class VisBView extends BorderPane {
 
 	
 	private void openVisB3DBrowser() {
-		createVis3DServer();
+		// createVis3DServer();
+		startServer();
 
 		if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
 			try {

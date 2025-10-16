65d7ca718816b920933a4eea99e69bf13f9ac43e
DerHyper <maxrichter15@web.de>
2025-10-16 20:27:43 +0200

    Add VisB3D DTO

---
+++ src/main/java/de/prob2/ui/visb/visb3d/VisB3DDto.java
@@ -0,0 +1,12 @@
+package de.prob2.ui.visb.visb3d;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VisB3DDto {
+    public List<VisB3DObjectDto> objectStates;
+
+    public VisB3DDto() {
+        this.objectStates = new ArrayList<>();
+    }
+}
---
+++ src/main/java/de/prob2/ui/visb/visb3d/VisB3DObjectDto.java
@@ -0,0 +1,30 @@
+package de.prob2.ui.visb.visb3d;
+
+public class VisB3DObjectDto {
+    public enum MaterialDto {
+        standard,
+        red,
+        green,
+        yellow,
+        blue,
+        metallic,
+        glassy,
+        transparent
+    }
+
+    public String name;
+    public MaterialDto material;
+    public Boolean isActive;
+    public Vector3Dto position;
+    public Vector3Dto rotation;
+    public Vector3Dto scale;
+
+    public VisB3DObjectDto(String name) {
+        this.name = name;
+        this.material = null;
+        this.isActive = null;
+        this.position = null;
+        this.rotation = null;
+        this.scale = null;
+    }
+}

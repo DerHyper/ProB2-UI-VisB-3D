0ee798eb8e7de59e089e2f19b793524892666d64
DerHyper <maxrichter15@web.de>
2025-10-16 18:04:10 +0200

    Add Logger to VisBWebSocketServer

--- src/main/java/de/prob2/ui/visb/VisBWebSocketServer.java
+++ src/main/java/de/prob2/ui/visb/VisBWebSocketServer.java
@@ -6,12 +6,15 @@ import java.nio.ByteBuffer;
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-/// Modified verion of the "Server Example" from 
+/// Modified verion of the "VisBWebSocketServe Example" from 
 /// https://github.com/TooTallNate/Java-WebSocket/wiki#server-example, 05.09.2025, Java-WebSocket Server Example
 public class VisBWebSocketServer extends WebSocketServer {
 
 	private static VisBWebSocketServer instance;
+	private static final Logger LOGGER = LoggerFactory.getLogger(VisBWebSocketServer.class);
 
 	public VisBWebSocketServer(InetSocketAddress address) {
 		super(address);
@@ -21,33 +24,35 @@ public class VisBWebSocketServer extends WebSocketServer {
 	@Override
 	public void onOpen(WebSocket conn, ClientHandshake handshake) {
 		conn.send("Welcome to the server!"); //This method sends a message to the new client
-		broadcast( "new connection: " + handshake.getResourceDescriptor() ); //This method sends a message to all clients connected
-		System.out.println("new connection to " + conn.getRemoteSocketAddress());
+		broadcast("new connection: " + handshake.getResourceDescriptor()); //This method sends a message to all clients connected
+		LOGGER.info("VisBWebSocketServe: New connection to {}", conn.getRemoteSocketAddress());
 	}
 
 	@Override
 	public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-		System.out.println("closed " + conn.getRemoteSocketAddress() + " with exit code " + code + " additional info: " + reason);
+		LOGGER.info(
+			"VisBWebSocketServe: Closed connection to {} with exit code {} additional info: {}",
+			conn.getRemoteSocketAddress(), code, reason);
 	}
 
 	@Override
 	public void onMessage(WebSocket conn, String message) {
-		System.out.println("received message from "	+ conn.getRemoteSocketAddress() + ": " + message);
+		LOGGER.info("VisBWebSocketServe: Received message from {}: {}", conn.getRemoteSocketAddress(), message);
 	}
 
 	@Override
 	public void onMessage(WebSocket conn, ByteBuffer message ) {
-		System.out.println("received ByteBuffer from "	+ conn.getRemoteSocketAddress());
+		LOGGER.info("VisBWebSocketServe: Received ByteBuffer from {}", conn.getRemoteSocketAddress());
 	}
 
 	@Override
 	public void onError(WebSocket conn, Exception ex) {
-		System.err.println("an error occurred on connection " + conn.getRemoteSocketAddress()  + ":" + ex);
+		LOGGER.error("VisBWebSocketServe: An error occurred on connection {}: {}", conn != null ? conn.getRemoteSocketAddress() : "Unknown", ex);
 	}
 	
 	@Override
 	public void onStart() {
-		System.out.println("server started successfully");
+		LOGGER.info("VisBWebSocketServe: Server started successfully");
 	}
 
 	/// WebSocket-Server (ws://localhost:8081/)

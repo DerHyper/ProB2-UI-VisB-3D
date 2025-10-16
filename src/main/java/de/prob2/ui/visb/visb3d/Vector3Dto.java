54fe6151a41588222c508ed3336fcd54fc2775ff
DerHyper <maxrichter15@web.de>
2025-10-16 20:27:15 +0200

    Add Vector3 DTO

---
+++ src/main/java/de/prob2/ui/visb/visb3d/Vector3Dto.java
@@ -0,0 +1,13 @@
+package de.prob2.ui.visb.visb3d;
+
+public class Vector3Dto {
+    public float x;
+    public float y;
+    public float z;
+
+    public Vector3Dto(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}

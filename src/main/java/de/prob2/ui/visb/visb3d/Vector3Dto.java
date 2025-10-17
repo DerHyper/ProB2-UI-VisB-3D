3e6d37efcff296a5af5f39af5200f4b848e91f28
DerHyper <maxrichter15@web.de>
2025-10-17 17:34:22 +0200

    Refactor Vector3Dto to allow null values

--- src/main/java/de/prob2/ui/visb/visb3d/Vector3Dto.java
+++ src/main/java/de/prob2/ui/visb/visb3d/Vector3Dto.java
@@ -1,9 +1,9 @@
 package de.prob2.ui.visb.visb3d;
 
 public class Vector3Dto {
-    public float x;
-    public float y;
-    public float z;
+    public Float x;
+    public Float y;
+    public Float z;
 
     public Vector3Dto(float x, float y, float z) {
         this.x = x;

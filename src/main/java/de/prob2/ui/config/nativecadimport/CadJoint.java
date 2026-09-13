33825937c1a81890ab8b0d7361d696855bcd50b6
DerHyper <maxrichter15@web.de>
2026-09-13 14:50:45 +0200

    Add CadJoint class

---
+++ src/main/java/de/prob2/ui/config/nativecadimport/CadJoint.java
@@ -0,0 +1,131 @@
+package de.prob2.ui.config.nativecadimport;
+
+public class CadJoint {
+
+    private Float angle;
+    private Float angleMax;
+    private Float angleMin;
+    private Boolean enableAngleMin;
+    private Boolean enableAngleMax;
+    private Float distance;
+    private Float distance2;
+    private Float lengthMin;
+    private Float lengthMax;
+    private Boolean enableLengthMin;
+    private Boolean enableLengthMax;
+
+    public CadJoint(
+            Float angle,
+            Float angleMax,
+            Float angleMin,
+            Boolean enableAngleMin,
+            Boolean enableAngleMax,
+            Float distance,
+            Float distance2,
+            Float lengthMin,
+            Float lengthMax,
+            Boolean enableLengthMin,
+            Boolean enableLengthMax) {
+
+        this.angle = angle;
+        this.angleMax = angleMax;
+        this.angleMin = angleMin;
+        this.enableAngleMin = enableAngleMin;
+        this.enableAngleMax = enableAngleMax;
+        this.distance = distance;
+        this.distance2 = distance2;
+        this.lengthMin = lengthMin;
+        this.lengthMax = lengthMax;
+        this.enableLengthMin = enableLengthMin;
+        this.enableLengthMax = enableLengthMax;
+    }
+
+    public Float getAngle() {
+        return angle;
+    }
+
+    public void setAngle(Float angle) {
+        this.angle = angle;
+    }
+
+    public Float getAngleMax() {
+        return angleMax;
+    }
+
+    public void setAngleMax(Float angleMax) {
+        this.angleMax = angleMax;
+    }
+
+    public Float getAngleMin() {
+        return angleMin;
+    }
+
+    public void setAngleMin(Float angleMin) {
+        this.angleMin = angleMin;
+    }
+
+    public Boolean getEnableAngleMin() {
+        return enableAngleMin;
+    }
+
+    public void setEnableAngleMin(Boolean enableAngleMin) {
+        this.enableAngleMin = enableAngleMin;
+    }
+
+    public Boolean getEnableAngleMax() {
+        return enableAngleMax;
+    }
+
+    public void setEnableAngleMax(Boolean enableAngleMax) {
+        this.enableAngleMax = enableAngleMax;
+    }
+
+    public Float getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Float distance) {
+        this.distance = distance;
+    }
+
+    public Float getDistance2() {
+        return distance2;
+    }
+
+    public void setDistance2(Float distance2) {
+        this.distance2 = distance2;
+    }
+
+    public Float getLengthMin() {
+        return lengthMin;
+    }
+
+    public void setLengthMin(Float lengthMin) {
+        this.lengthMin = lengthMin;
+    }
+
+    public Float getLengthMax() {
+        return lengthMax;
+    }
+
+    public void setLengthMax(Float lengthMax) {
+        this.lengthMax = lengthMax;
+    }
+
+    public Boolean getEnableLengthMin() {
+        return enableLengthMin;
+    }
+
+    public void setEnableLengthMin(Boolean enableLengthMin) {
+        this.enableLengthMin = enableLengthMin;
+    }
+
+    public Boolean getEnableLengthMax() {
+        return enableLengthMax;
+    }
+
+    public void setEnableLengthMax(Boolean enableLengthMax) {
+        this.enableLengthMax = enableLengthMax;
+    }
+}
+

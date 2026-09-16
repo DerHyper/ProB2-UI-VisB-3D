fe9341751937d552598a4626c24f6963e1e6ea91
DerHyper <maxrichter15@web.de>
2026-09-16 20:18:16 +0200

    Add FreeCadHandler

---
+++ src/main/java/de/prob2/ui/dataimport/nativecadimport/FreeCadHandler.java
@@ -0,0 +1,24 @@
+package de.prob2.ui.dataimport.nativecadimport;
+
+import org.xml.sax.helpers.DefaultHandler;
+
+public class FreeCadHandler extends DefaultHandler {
+    private static final String OBJECT = "Object";
+    private static final String EXTENTIONS = "Extensions";
+    private static final String EXTENTION = "Extension";
+    private static final String PROPERTIES = "Properties";
+    private static final String PROPERTY = "Property";
+    private static final String FLOAT = "Float";
+    private static final String BOOL = "Bool";
+    private static final String INTEGER = "Integer";
+    private static final String CUSTOMENUMLIST = "CustomEnumList";
+    private static final String ENUM = "Enum";
+    private static final String PROPERTYPLACEMENT = "PropertyPlacement";
+    private static final String XLINK = "XLink";
+    private static final String SUB = "Sub";
+    
+
+    private CadJointData jointData;
+    private CadJoint currentJoint;
+    private StringBuilder elementValue;
+}

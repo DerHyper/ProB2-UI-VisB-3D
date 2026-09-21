e69f3f7883e4b3ff9dc4b65584173243165bfda6
DerHyper <maxrichter15@web.de>
2026-09-21 17:47:53 +0200

    Add attributes and joint variables to FreeCadHandler

--- src/main/java/de/prob2/ui/dataimport/nativecadimport/FreeCadHandler.java
+++ src/main/java/de/prob2/ui/dataimport/nativecadimport/FreeCadHandler.java
@@ -3,6 +3,7 @@ package de.prob2.ui.dataimport.nativecadimport;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class FreeCadHandler extends DefaultHandler {
+    // Elements
     private static final String OBJECT = "Object";
     private static final String EXTENTIONS = "Extensions";
     private static final String EXTENTION = "Extension";
@@ -17,8 +18,24 @@ public class FreeCadHandler extends DefaultHandler {
     private static final String XLINK = "XLink";
     private static final String SUB = "Sub";
     
+    // Attributes
+    private static final String NAME = "name";
+    private static final String GROUP = "group";
+    private static final String VALUE = "value";
 
-    private ParseData parseData;
+    // FCStd Joint variables
+    private static final String ANGLE = "Angle";
+    private static final String ANGLEMAX = "AngleMax";
+    private static final String ANGLEMIN = "AngleMin";
+    private static final String DISTANCE = "Distance";
+    private static final String DISTANCE2 = "Distance2";
+    private static final String ENABLEANGLEMAX = "EnableAngleMax";
+    private static final String ENABLEANGLEMIN = "EnableAngleMin";
+    private static final String ENABLELENGTHMAX = "EnableLengthMax";
+    private static final String ENABLELENGTHMIN = "EnableLengthMin";
+    
+    // Local variables
+    private ParseData parseData = new ParseData();
     private CadJoint currentJoint;
     private StringBuilder elementValue;
 }
